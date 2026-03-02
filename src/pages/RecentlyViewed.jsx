91b9e7c95906b59b4d4c237951d0b0ed2797e19c
Birmingham, Aidan <aidanbirmingham@my.unt.edu>
2026-03-02 13:50:57 -0600

    Added decoder for recentlyViewedRecipes in localStorage

--- src/pages/RecentlyViewed.jsx
+++ src/pages/RecentlyViewed.jsx
@@ -7,7 +7,21 @@ import FavoriteButton from '../components/FavoriteButton';
 import './RecentlyViewed.css';
 
 const RecentlyViewed = () => {
-  
+    
+
+    const getRecipeFromIndex = (index) => {
+        return localStorage.getItem('recentlyViewed').substring(index * 7, (index + 1) * 7);
+    }
+
+    //  Recently viewed recipes will be stored in localStorage with the key 'recentlyViewed', they will
+    //  be stored in the format of 'xxxxxx,xxxxxx,xxxxxx ...', where index 0 of that string is the 6
+    //  digit recipe id of the most recently viewed recipe. 
+    //
+    //  Above is a function to decode recently viewed recipes from localStorage
+
+    console.log(getRecipeFromIndex(0));
+    console.log(getRecipeFromIndex(2));
+    
 };
 
 export default RecentlyViewed;
